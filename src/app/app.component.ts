b014672be6ea867871e1a0994c0a527cf03c6678
berci160 <ilyesberci@gmail.com>
2024-05-12 14:04:47 +0200

    Javítás

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'internship-angular-test-2024';
+title = 'internship-angular-test-2024';
+counter:number=0;
+
+public increase()
+{
+  this.counter++;
+}
+public decrease()
+{ if(this.counter>0){
+  this.counter--;
+}
+}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SandboxComponent } from './Pages/sandbox/sandbox.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', redirectTo:'sandbox', pathMatch:'full'},
+  {path: 'sandbox' , component:SandboxComponent }
+];
